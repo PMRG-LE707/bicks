ad1cf962aa1625d171d5e5a5e8a6ad1b18ccdfc2
CWXie <20175947@cqu.edu.cn>
2021-04-27 16:31:35 +0800

    a new file named test for 'test'

---
+++ BICK/test.py
@@ -0,0 +1,27 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Apr 19 15:29:16 2021
+
+@author: CWXie
+"""
+from PhotonicCrystalSlab import PhotonicCrystalSlab, EssentialNumber
+from bicky import FindBICs
+import time
+
+t1 = time.time()
+
+a = 1
+fr = 0.5
+ep = [1, 4.9]
+phcs = PhotonicCrystalSlab(ep, fr, a)
+num = EssentialNumber(n_radiation=1)
+
+fb = FindBICs(phcs, num)
+fb.getcoeffs()
+
+t2 = time.time()
+print(t2 - t1)
+hstart = 1.0
+hend = 3.0
+Nh = 20
+fb.run(hstart, hend, Nh)
